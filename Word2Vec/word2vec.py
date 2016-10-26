b24bf8f9f742c20627558c55b31c601b03c4001c
jmoore523 <jmoore523@gmail.com>
2016-10-26 08:39:18 -0700

    first trial

---
+++ Word2Vec/word2vec.py
@@ -0,0 +1,58 @@
+import glob, os
+import gensim
+from bs4 import BeautifulSoup
+import nltk.data
+
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+def file_to_soup(filename):
+    """
+    """
+    html_report_part1 = open(filename,'r')
+    soup = BeautifulSoup(html_report_part1, "xml")
+    return soup
+
+def soup_to_sentences(soup):
+    """
+    """
+    sentence_list = []
+    contents =  soup.find_all("body.content")
+    for content in contents:
+        blocks = content.find_all("block")
+        for block in blocks:
+            if str(block['class']) == "full_text":
+                paragraphs = block.find_all("p")
+                for p in paragraphs:
+                    p = str(p).replace("<p>","").replace("</p>","")
+                    sentences = tokenizer.tokenize(p)
+                    for sentence in sentences:
+                        sentence_list.append(sentence.split())
+    return sentence_list
+
+# Set-up sentences
+os.chdir("/Users/jmoore523/Documents/01/")
+sentences = []
+for root, dirs, files in os.walk("."):
+    path = root.split('/')
+    for file in files:
+        if file.endswith(".xml"):
+            soup = file_to_soup("./" + str(os.path.basename(root)) + "/" + str(file))
+            for element in soup_to_sentences(soup):
+                sentences.append(element)
+
+# Build word2vec model
+model = gensim.models.Word2Vec(sentences)
+
+# Test word2vec model
+print "Similarlity: Woman & Man"
+print model.similarity('woman', 'man')
+print "Similarlity: Even & Odd"
+print model.similarity('even', 'odd')
+print "Similarlity: Shoe & Business"
+print model.similarity('shoe', 'business')
+print "Similarlity: Business & Woman"
+print model.similarity('business', 'woman')
+print "Similarlity: Business & Revenue"
+print model.similarity('business', 'revenue')
+print "Similarlity: Job & Career"
+print model.similarity('job', 'career')
