b7eabc8fdcc4156c70ca329d920dc06693efaa41
jmoore523 <jmoore523@gmail.com>
2016-11-27 14:52:58 -0800

    shell script for author gender w2v

---
+++ Word2Vec/author_genders.sh
@@ -0,0 +1,59 @@
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1987/" -m "/Volumes/My Book/W2VModels/GenderModels/1987M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1987_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1987/" -m "/Volumes/My Book/W2VModels/GenderModels/1987F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1987_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1988/" -m "/Volumes/My Book/W2VModels/GenderModels/1988M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1988_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1988/" -m "/Volumes/My Book/W2VModels/GenderModels/1988F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1988_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1989/" -m "/Volumes/My Book/W2VModels/GenderModels/1989M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1989_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1989/" -m "/Volumes/My Book/W2VModels/GenderModels/1989F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1989_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1990/" -m "/Volumes/My Book/W2VModels/GenderModels/1990M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1990_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1990/" -m "/Volumes/My Book/W2VModels/GenderModels/1990F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1990_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1991/" -m "/Volumes/My Book/W2VModels/GenderModels/1991M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1991_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1991/" -m "/Volumes/My Book/W2VModels/GenderModels/1991F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1991_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1992/" -m "/Volumes/My Book/W2VModels/GenderModels/1992M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1992_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1992/" -m "/Volumes/My Book/W2VModels/GenderModels/1992F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1992_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1993/" -m "/Volumes/My Book/W2VModels/GenderModels/1993M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1993_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1993/" -m "/Volumes/My Book/W2VModels/GenderModels/1993F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1993_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1994/" -m "/Volumes/My Book/W2VModels/GenderModels/1994M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1994_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1994/" -m "/Volumes/My Book/W2VModels/GenderModels/1994F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1994_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1995/" -m "/Volumes/My Book/W2VModels/GenderModels/1995M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1995_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1995/" -m "/Volumes/My Book/W2VModels/GenderModels/1995F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1995_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1996/" -m "/Volumes/My Book/W2VModels/GenderModels/1996M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1996_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1996/" -m "/Volumes/My Book/W2VModels/GenderModels/1996F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1996_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1997/" -m "/Volumes/My Book/W2VModels/GenderModels/1997M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1997_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1997/" -m "/Volumes/My Book/W2VModels/GenderModels/1997F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1997_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1998/" -m "/Volumes/My Book/W2VModels/GenderModels/1998M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1998_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1998/" -m "/Volumes/My Book/W2VModels/GenderModels/1998F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1998_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1999/" -m "/Volumes/My Book/W2VModels/GenderModels/1999M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1999_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/1999/" -m "/Volumes/My Book/W2VModels/GenderModels/1999F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/1999_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2000/" -m "/Volumes/My Book/W2VModels/GenderModels/2000M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2000_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2000/" -m "/Volumes/My Book/W2VModels/GenderModels/2000F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2000_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2001/" -m "/Volumes/My Book/W2VModels/GenderModels/2001M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2001_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2001/" -m "/Volumes/My Book/W2VModels/GenderModels/2001F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2001_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2002/" -m "/Volumes/My Book/W2VModels/GenderModels/2002M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2002_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2002/" -m "/Volumes/My Book/W2VModels/GenderModels/2002F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2002_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2003/" -m "/Volumes/My Book/W2VModels/GenderModels/2003M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2003_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2003/" -m "/Volumes/My Book/W2VModels/GenderModels/2003F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2003_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2004/" -m "/Volumes/My Book/W2VModels/GenderModels/2004M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2004_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2004/" -m "/Volumes/My Book/W2VModels/GenderModels/2004F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2004_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2005/" -m "/Volumes/My Book/W2VModels/GenderModels/2005M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2005_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2005/" -m "/Volumes/My Book/W2VModels/GenderModels/2005F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2005_gender_of_writer.tsv" -as "female"
+
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2006/" -m "/Volumes/My Book/W2VModels/GenderModels/2006M.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2006_gender_of_writer.tsv" -as "male"
+python word2vec_stream_authorgender.py -d "/Volumes/My Book/NYTCorpus/data/2006/" -m "/Volumes/My Book/W2VModels/GenderModels/2006F.w2v" -af "/Users/jmoore523/Dropbox/Graduate_School/MSE231/MSE231-Project/gender-of-writer/2006_gender_of_writer.tsv" -as "female"
